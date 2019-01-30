88fb81d37b3a8963756650a38e740855b6d57ace
Angelina Montanez <angelinamontanez80@gmail.com>
2019-01-30 17:38:02 +0000

    Automatically backed up by Learn

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -1,9 +1,6 @@
 describe '#meal_choice' do
   it 'should default to meat' do
-  def some_method (meal_choice)("meat")
+  def meal_choice (meal_choice)("meat")
   end
 
-  it 'should allow you to set a meal' do
-  def  some_method (meal_choice("meat")("meat")
-  end
-end
+  
